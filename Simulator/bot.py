9ca796fca2eed47b76fd318224fc4b1e6a9248ce
junjinyong <junjinyong@snu.ac.kr>
2023-05-19 00:54:59 +0900

    hash function implemented

--- Simulator/bot.py
+++ Simulator/bot.py
@@ -1,23 +1,23 @@
-class Bot():
-    def __init__(self, bot_ip, local):
+class Bot:
+    def __init__(self, bot_ip, local, threshold):
         self.bot_ip = bot_ip
         self.local = local
-        
+
         self.nonce = 0
         self.time = 0
-        self.threshold = 0
+        self.threshold = threshold
         self.token = 0
-    
+
     def get_puzzle_token(self, local):
         self.token = local.send_puzzle_token()
-        
+
     def hash_ftn(self):
         return self.local.hash_ftn(self)
-    
+
     def solve_puzzle(self):
         self.nonce = 0
-        for i in range(0, 2147483647):
-            if self.hash_ftn() <= self.threshold:
+        for i in range(2147483648):
+            if self.hash_ftn() < self.threshold:
+                print("Bot", self.bot_ip, "solved the puzzle with nonce", self.nonce)
                 break
             self.nonce += 1
-            
--- Simulator/main.py
+++ Simulator/main.py
@@ -1,6 +1,26 @@
 from threading import Thread
 from multiprocessing import Process, Queue
 
+from bot import Bot
+from auth import Auth
+from target import Target
+from local import Local
+
 if __name__ == "__main__":
-    for i in range(100):
-        Bot[i]
+    local_ip = 1015
+    auth_ip = 1025
+    target_ip = 1035
+    chain_len = 100
+    bot_num = 20
+    threshold = 1000
+    flag = False
+
+    target = Target(target_ip, flag, threshold)
+    auth = Auth(auth_ip, target)
+    local = Local(local_ip, auth, chain_len)
+    bot_list = [Bot(x, local, threshold) for x in range(bot_num)]
+
+
+    for bot in bot_list:
+        bot.solve_puzzle()
+        target.validate_puzzle(bot)
